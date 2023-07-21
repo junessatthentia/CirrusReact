993d3074491dbcd9aa162b4300787586b110e9c8
junniexbee <hello@junniexbee.com>
2023-07-21 15:51:07 -0400

    Update Button.stories.ts

--- src/stories/Button.stories.ts
+++ src/stories/Button.stories.ts
@@ -1,11 +1,11 @@
 import type { Meta, StoryObj } from '@storybook/react';
 
-import { Button } from './Button';
+import { tcButton } from './Button';
 
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 const meta = {
   title: 'Example/Button',
-  component: Button,
+  component: tcButton,
   parameters: {
     // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/react/configure/story-layout
     layout: 'centered',
@@ -16,7 +16,7 @@ const meta = {
   argTypes: {
     backgroundColor: { control: 'color' },
   },
-} satisfies Meta<typeof Button>;
+} satisfies Meta<typeof tcButton>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
